c1f47267c29f7af34233747f2bba2111d4578547
wu tai <tai.wu@sap.com>
2019-09-15 00:03:24 +0800

    add static setting

--- pythonweb/rasiberryPiWebManager/urls.py
+++ pythonweb/rasiberryPiWebManager/urls.py
@@ -17,7 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+STATIC_URL = '/static/'
+
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path(r'api/v1/gpio/', include('gpioAPI.urls')),
+    static(settings.STATIC_URL, document_root = settings.STATIC_ROOT )
 ]
