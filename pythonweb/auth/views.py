35d85f20e47646bf71b13ba1b20ec753b0f0595b
wu tai <tai.wu@sap.com>
2019-09-15 16:54:37 +0800

    add auth api

---
+++ pythonweb/auth/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import json
+
+# Create your views here.
+def login():
+  loginData = {
+    'id': 'admin',
+    'name': 'onwebbe',
+    'username': 'admin',
+    'password': '',
+    'avatar': 'https://gw.alipayobjects.com/zos/rmsportal/jZUIxmJycoymBprLOUbT.png',
+    'status': 1,
+    'telephone': '',
+    'lastLoginIp': '27.154.74.117',
+    'lastLoginTime': 1534837621348,
+    'creatorId': 'admin',
+    'createTime': 1497160610259,
+    'deleted': 0,
+    'roleId': 'admin',
+    'lang': 'zh-CN',
+    'token': '4291d7da9005377ec9aec4a71ea837f'
+  }
+  return json.dumps(loginData, indent=4)
